68b52e8bc15aee146a2d19a073eb4568da1528f0
Sam Moore <sammooresemail23@gmail.com>
2022-07-11 00:39:37 -0700

    questions array

--- Assets/script.js
+++ Assets/script.js
@@ -1,44 +1,22 @@
-//variable declarations//
-var choiceA = A;
-var choiceB = B;
-var choiceC = C;
-var choiceD = D;
-var q1 = [{}];
-var q2 = [{}];
-var q3 = [{}];
-var q4 = [{}];
-var quizQuestions = [q1, q2, q3, q4,];
-var answerChoices = [A, B, C, D];
-
-//create buttons for answer choices (A-D)
-A = document.createElement("div"); 
-
-B = 
-
-C =
-
-D =
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var actionContainerEl = document.createElement("div");
-// actionContainerEl.className = "task-actions";
-// var editButtonEl = document.createElement("button");
-// editButtonEl.textContent = "Edit";
-// editButtonEl.className = "btn edit-btn";
-// editButtonEl.setAttribute("data-task-id", taskId);
-
-// actionContainerEl.appendChild(editButtonEl);
-
-// if statements to animate the quiz questions//
-// if ()
+const questions = [
+    {
+        question: "The div element is what exactly?",
+        choices: ["A a container", "B a divider", "C a function", "D a page"],
+        answer: "A a container"
+    },
+    {
+        question: "What symbol represents a terminator?",
+        choices: ["A a ';'", "B a '%'", "C a '&'", "D a '*'"],
+        answer: "A a ';'"
+    },
+    {
+        question: "I'm glad I have _______ to apply the functionality.",
+        choices: ["A HTML", "B CSS", "C  Javascript", "D Java"],
+        answer: "C Javascript"
+    }
+    {
+        question: "What does the var command do?",
+        choices: ["A  creates a document", "B creates a variable", "C creates a function", "creates parameters"],
+        answer: "B creates a variable"
+    }]
+    
