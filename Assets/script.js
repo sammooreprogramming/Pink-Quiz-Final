fa96dfd2a06d283246b3dc0c4636263ac653ce84
Sam Moore <sammooresemail23@gmail.com>
2022-07-10 18:31:00 -0700

    variables in place

---
+++ Assets/script.js
@@ -0,0 +1,44 @@
+//variable declarations//
+var choiceA = A;
+var choiceB = B;
+var choiceC = C;
+var choiceD = D;
+var q1 = [{}];
+var q2 = [{}];
+var q3 = [{}];
+var q4 = [{}];
+var quizQuestions = [q1, q2, q3, q4,];
+var answerChoices = [A, B, C, D];
+
+//create buttons for answer choices (A-D)
+A = document.createElement("div"); 
+
+B = 
+
+C =
+
+D =
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var actionContainerEl = document.createElement("div");
+// actionContainerEl.className = "task-actions";
+// var editButtonEl = document.createElement("button");
+// editButtonEl.textContent = "Edit";
+// editButtonEl.className = "btn edit-btn";
+// editButtonEl.setAttribute("data-task-id", taskId);
+
+// actionContainerEl.appendChild(editButtonEl);
+
+// if statements to animate the quiz questions//
+// if ()
