d559cb5237a5001e8e45c948bc0d7e7be9c566fa
Sam Moore <sammooresemail23@gmail.com>
2022-07-11 20:22:35 -0700

    early development and variable dec

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
-const questions = [
+// Questions Array
+let questions = [
     {
         question: "The div element is what exactly?",
         choices: ["A a container", "B a divider", "C a function", "D a page"],
@@ -13,10 +14,59 @@ const questions = [
         question: "I'm glad I have _______ to apply the functionality.",
         choices: ["A HTML", "B CSS", "C  Javascript", "D Java"],
         answer: "C Javascript"
-    }
+    },
     {
         question: "What does the var command do?",
         choices: ["A  creates a document", "B creates a variable", "C creates a function", "creates parameters"],
         answer: "B creates a variable"
     }]
     
+// define variables for later use
+    const homepage = document.querySelector("#homepage-card");
+    const quiz = document.querySelector("#quiz-card");
+    const highscore = document.querySelector("#highscore-card");
+    const startButton = document.querySelector("#Start-Btn");
+    let A = document.querySelector ("#A");
+    let B = document.querySelector ("#B");
+    let C = document.querySelector ("#C");
+    let D = document.querySelector ("#D");
+    let clock = document.querySelector( "#clock") 
+
+
+// All functions for logic
+function startTimedQuiz() {
+
+}     
+
+function deductTime() {
+
+}
+
+function startQuiz() {
+
+}
+
+function endQuiz() {
+
+}
+
+function hideAll() {
+
+}
+
+function displayEachQuestion() {
+
+}
+
+function correctAnswerChoices() {
+        
+}
+
+
+//logical statements
+
+
+
+
+//set time left as the score
+
