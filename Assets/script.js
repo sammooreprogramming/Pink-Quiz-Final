9143c9754ed214e71fc77fbd86f14a68d3ca7457
Sam Moore <sammooresemail23@gmail.com>
2022-09-09 20:05:54 -0700

    fixed much that was very wrong indeed

--- Assets/script.js
+++ Assets/script.js
@@ -3,95 +3,51 @@ let questionsArray = [
     {
         question: "The div element is what exactly?",
         choices: ["A a container", "B a divider", "C a function", "D a page"],
-        answer: "A a container"
+        correct: "A a container"
+        
     },
     {
         question: "What symbol represents a terminator?",
         choices: ["A a ';'", "B a '%'", "C a '&'", "D a '*'"],
-        answer: "A a ';'"
+        correct: "A a ';'"
     },
     {
         question: "I'm glad I have _______ to apply the functionality.",
         choices: ["A HTML", "B CSS", "C  Javascript", "D Java"],
-        answer: "C Javascript"
+        correct: "C Javascript"
     },
     {
         question: "What does the var command do?",
         choices: ["A  creates a document", "B creates a variable", "C creates a function", "creates parameters"],
-        answer: "B creates a variable"
+        correct: "B creates a variable"
     }];
     
 // define variables for cards to display later
-    const homepage = document.querySelector("#homepage-card");
-    const quiz = document.querySelector("#quiz-card");
-    const highscore = document.querySelector("#highscore-card");
-    const endPage = document.querySelector("#finish-page-card");
+    const homepage = document.getElementById("#homepage-card");
+    const quiz = document.getElementById("#quiz-card");
+    const highscore = document.getElementById("#highscore-card");
+    const endPage = document.getElementById("#finish-page-card");
 // link buttons for later logical use
-    let startButton = document.querySelector("#Start-Btn");
-    let A = document.querySelector ("#A");
-    let B = document.querySelector ("#B");
-    let C = document.querySelector ("#C");
-    let D = document.querySelector ("#D");
+    let startButton = document.getElementById("#Start-Btn");
+    let A = document.getElementById ("#A");
+    let B = document.getElementById ("#B");
+    let C = document.getElementById ("#C");
+    let D = document.getElementById ("#D");
 // time variables for function use
-    let clock = document.querySelector( "#clock") 
+    let clock = document.getElementById( "#clock") 
     let home = document.getElementById("homepage")
     let timeRemaining = 60;
 // index variables
     let questionNumber = 0;
 
-    
-//display the homepage to begin!
-    displayHomepage();
-
 // event listeners for the start button & the answer choices
-startButton.addEventListener("click", startQuiz);
 A.addEventListener("click", renderChoiceA);
 B.addEventListener("click", renderChoiceB);
 C.addEventListener("click", renderChoiceC);
 D.addEventListener("click", renderChoiceD);
 
-    
-// The function below formats the quiz and starts the clock, as well as displays the first question.
-function startQuiz () {
-        displayQuiz();
-    let clockLeft = setInterval(function () {
-        timeRemaining--;
-        clock.textContent = `Time: ${timeRemaining} seconds left!`;
-    
-        if (timeRemaining === 0) {
-            clearInterval(clockLeft);
-        };
-    }, 1000);
-};
+startButton.addEventListener("click", startQuiz);
 
+// functions
 
-
-
-function displayHomepage () {
-    homepage.style.display = "block";
-    highscore.style.display = "none";
-    endPage.style.display = "none";
-    quiz.style.display = "none";
-}
-
-function displayHighscore () {
-    homepage.style.display = "none";
-    highscore.style.display = "block";
-    endPage.style.display = "none";
-    quiz.style.display = "none";
-}
-
-function displayEndPage () {
-    homepage.style.display = "none";
-    highscore.style.display = "none";
-    endPage.style.display = "block";
-    quiz.style.display = "none";
-}
-
-function displayQuiz () {
-    homepage.style.display = "none";
-    highscore.style.display = "none";
-    endPage.style.display = "none";
-    quiz.style.display = "block";
-}
 
