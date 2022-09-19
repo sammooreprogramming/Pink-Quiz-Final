601ed832c11e4524986b9269d81bab5b419889f3
Sam Moore <sammooresemail23@gmail.com>
2022-09-19 16:49:01 -0700

    major html rework progress

--- Assets/script.js
+++ Assets/script.js
@@ -23,82 +23,43 @@ let questionsArray = [
     }];
     
 // define variables for cards to display later
-    const homepage = document.getElementById("#homepage-card");
-    const quiz = document.getElementById("#quiz-card");
-    const highscore = document.getElementById("#highscore-card");
-    const endPage = document.getElementById("#finish-page-card");
+    const homepage = document.querySelector("#Homepage");
+    const quiz = document.querySelector("#Quiz");
+    const highscore = document.querySelector("#Leaderboard");
+    const endPage = document.querySelector("#Endpage");
 // link buttons for later logical use
-    let startButton = document.getElementById("#Start-Btn");
-    let A = document.getElementById ("#A");
-    let B = document.getElementById ("#B");
-    let C = document.getElementById ("#C");
-    let D = document.getElementById ("#D");
+    let startButton = document.querySelector("#Start-Btn");
+    let A = document.querySelector ("#buttonA");
+    let B = document.querySelector ("#buttonB");
+    let C = document.querySelector ("#buttonC");
+    let D = document.querySelector ("#buttonD");
+    let endButton = document.querySelector ("#EndButton");
 // time variables for function use
-    let clock = document.getElementById( "#clock") 
-    let home = document.getElementById("homepage")
+    let clock = document.querySelector("#clock") 
+    let home = document.querySelector("homepage")
     let timeRemaining = 60;
 // index variables
     let questionNumber = 0;
     let liveClock;
     let correctChoice;
     
-// 
-    function startQuiz() {
+// i'm working
 
-    };
+startButton.addEventListener("click", startQuiz);
 
+A.style.display = "none";
+B.style.display = "none";
+C.style.display = "none";
+D.style.display = "none";
 
-    function renderChoices() {
+function startQuiz() {
+    
+homepage.style.display = "none";
+quiz.style.display = "block";
 
-        renderChoiceA() {
-            if (choice === A && A === correct) {
-                quiz.innerHTML = "Correct choice!";
-        } else {
-            quiz.innerHTML = "Incorrect choice!";
-        }
-    }
-
-        renderChoiceB() {
-            if (choice === B && B === correct) {
-                quiz.innerHTML = "Correct choice!";
-        } else {
-            quiz.innerHTML = "Incorrect choice!";
-        }
-    }
-
-        renderChoiceC() {
-            if (choice === C && C === correct) {
-                quiz.innerHTML = "Correct choice!";
-        } else {
-            quiz.innerHTML = "Incorrect choice!";
-        }
-    }
-
-        renderChoiceD() {
-            if (choice === D && D === correct) {
-                quiz.innerHTML = "Correct choice!";
-        } else {
-            quiz.innerHTML = "Incorrect choice!";
-        }
-    }
-};
-
-
-
-
-
-
-
-
-function renderChoiceA() {
-}
-
-function renderChoiceB() {
-}
-
-function renderChoiceC() {
-}
-
-function renderChoiceD() {
+A.style.display = "block";
+B.style.display = "block";
+C.style.display = "block";
+D.style.display = "block";
 }
 
